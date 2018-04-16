4b8abbfb63bd93e12f0af47d3d343baebac0ea9d
vanesantillana <vanessa.santillana@ucsp.edu.pe>
2018-04-16 13:40:52 -0500

    agrego tabla de simbolos

--- analexico.py
+++ analexico.py
@@ -1,8 +1,11 @@
 import ply.lex as lex
+import re
 import codecs
+from colorama import Fore
 
 resultado_lexema=[]
-cadena=''
+cadena_result=''
+tabla=[]
 
 reservada = (
     # Palabras Reservadas
@@ -11,12 +14,9 @@ reservada = (
     'FROM',
     'WHERE',
     'SET',
+    'INTO',
     'JOIN',
-    'INT',
-    'DOUBLE',
-    'VARCHAR',
-    'DATE',
-    'BOOLEAN'
+    'ON'
 )
 tokens = reservada + (
     'ID',
@@ -41,6 +41,7 @@ tokens = reservada + (
     'TODO',
 
     # Otros
+    'COMILLA',
     'PUNTOCOMA',
     'PUNTO',
     'COMA',
@@ -48,7 +49,6 @@ tokens = reservada + (
     'LLAVE',
     'COMENTARIO',
     'COMENTARIO_BLOCK'
-
 )
 
 # Reglas de Expresiones Regualres para token de Contexto simple
@@ -59,16 +59,24 @@ t_ASIGNAR = r'='
 t_OP_R = r'((<|>|=)=?)'
 t_TODO = r'\*'
 
-t_PUNTOCOMA = ';'
+t_COMILLA = r'(\'|")'
+t_PUNTOCOMA = r';'
 t_COMA = r','
+t_PUNTO = r'\.'
 t_PARENTESIS = r'(\(|\))'
 t_LLAVE= r'({|})'
 
 t_ignore =' \t'
 
+def t_ON(t):
+    r'on'
+    return t
+def t_INTO(t):
+    r'into'
+    return t
 def t_FECHA(t):
     r'([0-2][0-9]|3[0-1])(-|/)(0[1-9]|1[0-2])(-|/)(\d{4})'
-    return t;
+    return t
 def t_BOOL(t):
     r'(true|false)'
     return t
@@ -93,7 +101,7 @@ def t_ID(t):
     return t
 
 def t_NUMERO(t):
-    r'[0-9]+([,][0-9]+)?'
+    r'-?\d+([,]\d+)?'
     t.value = int(t.value)
     return t
 
@@ -126,29 +134,30 @@ def t_error( t):
     t.lexer.skip(1)
 
 def imprimir(res):
-    for i in resultado_lexema:
-        print(i)
+    for i in res:
+        if(i[:2]=="**"):
+            print(Fore.RED+i)
+        else:
+            print(Fore.BLACK+i)
 
-# Prueba de ingreso
-def prueba(data):
-    analizador = lex.lex()
-    analizador.input(data)
+def agregar_tabla_simbolos(t,val):
+    num=''
+    if(re.search(r'-?\d+',val)):
+        num='int'
+    if(t=='ID'):
+        tabla.append("|{:4}\t|{:5}\t\t|{:4}\t\t|".format(t,val, 'string'))
+    elif(t=='NUMERO'):
+        tabla.append("|{:4}\t|{:5}\t\t|{:4}\t\t|".format(t,val, num))
+    elif(t=='FECHA'):
+        tabla.append("|{:4}\t|{:5}\t\t|{:4}\t\t|".format(t,val, 'date'))
+    elif(t=='BOOL'):
+        tabla.append("|{:4}\t|{:5}\t\t|{:4}\t\t|".format(t,val, 'bool'))
 
-    resultado_lexema.clear()
-    while True:
-        tok = analizador.token()
-        if not tok:
-            break
-        # print("lexema de "+tok.type+" valor "+tok.value+" linea "tok.lineno)
-        estado = "Linea {:4} Tipo {:16} Valor {:16} Posicion {:4}".format(str(tok.lineno),str(tok.type) ,str(tok.value), str(tok.lexpos) )
-        resultado_lexema.append(estado)
-    return resultado_lexema
-
- # instanciamos el analizador lexico
+# instanciamos el analizador lexico
 analizador = lex.lex()
 
 if __name__ == '__main__':
-    directorio = '/home/vye/Documentos/2018-01/Compiladores/ALexico/'
+    directorio = '../ALexico/'
     archivo = 'test.vye'
     test = directorio+archivo
     fp =codecs.open(test,'r','utf-8')
@@ -162,7 +171,15 @@ if __name__ == '__main__':
         # print("lexema de "+tok.type+" valor "+tok.value+" linea "tok.lineno)
         estado = "Linea {:4} Tipo {:16} Valor {:16} Posicion {:4}".format(str(tok.lineno),str(tok.type) ,str(tok.value), str(tok.lexpos) )
         resultado_lexema.append(estado)
+        cadena_result+= str(tok.type)+" ";
+        agregar_tabla_simbolos(str(tok.type) ,str(tok.value))
     imprimir(resultado_lexema)
+    print(Fore.YELLOW+"\nANALISIS LEXICO:\n"+Fore.BLUE + cadena_result)
+    print(Fore.GREEN+"TABLA DE SIMBOLOS\n"+"|Id\t|Nombre\t\t|Tipo\t\t|")
+    tabla_simb = set(tabla)
+    imprimir(tabla_simb)
+
+
 '''
 data = input("Ingrese: ")
 prueba(data)
